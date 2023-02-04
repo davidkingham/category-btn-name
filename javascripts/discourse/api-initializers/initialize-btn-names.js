06da85aa80502bbe32de72d799d2d27bc2c80fd0
davidkingham <35718296+davidkingham@users.noreply.github.com>
2023-02-03 21:36:37 -0700

    Update initialize-btn-names.js

--- javascripts/discourse/api-initializers/initialize-btn-names.js
+++ javascripts/discourse/api-initializers/initialize-btn-names.js
@@ -5,6 +5,7 @@ const TRANSLATION_KEYS = {
   "Image Critiques": "critiques",
   "Beginners": "beginners",
   "Discussions": "discussions",
+  "Introductions": "introductions",
 };
 
 export default apiInitializer("0.8", (api) => {
